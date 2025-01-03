50dc0be4472ee70dfd1ad7251ecb05b9e9da69f6
Jalal <128756237+JalalA984@users.noreply.github.com>
2025-01-03 16:39:37 -0500

    create serverError helper func

---
+++ internal/handlers/helpers.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+
+	"github.com/JalalA984/apptrak/pkg/config"
+)
+
+func serverError(app *config.Application) func(res http.ResponseWriter, err error) { 
+	return func(res http.ResponseWriter, err error) {
+		stackTrace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+		app.ErrorLog.Print(stackTrace)
+
+		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+
+	}
+	
+}
