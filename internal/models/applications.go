2fe4742bd9d391386a9744dc8fc4bb5c89675a10
Jalal <128756237+JalalA984@users.noreply.github.com>
2025-01-05 16:50:29 -0500

    setup initial db model

--- internal/models/applications.go
+++ internal/models/applications.go
@@ -1,4 +1,33 @@
 package models
 
+import (
+	"database/sql"
+	"time"
+)
+
 type Application struct {
+	ID          int
+	Name        string
+	CompanyName string
+	Position    string
+	CreatedAt   time.Time
+}
+
+type ApplicationModel struct {
+	DB *sql.DB
+}
+
+// Insert new application to DB
+func (m *ApplicationModel) Insert(name string, companyName string, position int) (int, error) {
+	return 0, nil
+}
+
+// Return specific application based on ID
+func (m *ApplicationModel) Get(id int) (*Application, error) {
+	return nil, nil
+}
+
+// Return 10 most recently created applications
+func (m *ApplicationModel) Latest() ([]*Application, error) {
+	return nil, nil
 }
